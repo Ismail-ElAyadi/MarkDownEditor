ecb864a547e380766ca9a624d2d7578b5f4fe693
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-24 00:09:55 +0200

    Created App's Body

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -3,8 +3,9 @@ import React, { Component} from 'react'
 export default class Body extends Component {
     render() {
         return (
-            <section>
-
+            <section className="container">
+                    <div className="col-md-6"></div>
+                    <div className="col-md-6"></div>
             </section>
         )
     }
