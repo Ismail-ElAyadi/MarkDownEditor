e5e9e0b900788826dfa3b3551cb1618c6fcbd3da
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-24 22:38:07 +0200

    add some style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import Header from './component/Header'
 import Footer from './component/Footer'
+import Body from './component/Body'
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
         <Header/>
+        <Body/>
         <Footer/>
       </Fragment>
     )
--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,12 +1,22 @@
-import React, { Component} from 'react'
+import React, { Component } from "react";
 
 export default class Body extends Component {
-    render() {
-        return (
-            <section className="container">
-                    <div className="col-md-6"></div>
-                    <div className="col-md-6"></div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section className="p-0 d-flex">
+        <div className="col-md-5  block">
+          <h2>Editor</h2>
+          <textarea
+            className="col-md-12 "
+          ></textarea>
+        </div>
+        <div className="col-md-5 block ">
+          <h2>Preview</h2>
+          <textarea
+            className="col-md-12"
+          ></textarea>
+        </div>
+      </section>
+    );
+  }
 }
