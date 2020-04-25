c8df41d1b02fa9e72eb8e46b4c4e4dc52222099e
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-25 07:50:47 +0200

    little Modification for instructions

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
                 <footer className="col-12 bg-warning d-flex justify-content-between">
-                    <p>Notice : Let 2 spaces to go on the next line.</p>
+                    <p>Notice : Let 2 spaces at the end of the line to go on the next line.</p>
                 <p>
                     By
                     <a href={infos.links.linkedin} target="_blank" rel="author noopener noreferrer "> {infos.nom} </a>
