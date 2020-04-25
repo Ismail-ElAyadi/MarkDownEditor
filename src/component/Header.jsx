b6704ba9e546ecfa42f51b35cbcc0678d0ddbb54
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-25 07:49:02 +0200

    Add Method in Compenent

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,16 +1,17 @@
 import React, { Component} from 'react'
 
 export default class Header extends Component {
+
     render() {
         return (
-                <nav className="col-md-12 bg-dark d-flex flex-row align-content-center">
-                <h1 className="col-md-4 ">MarkDown Editor</h1>
-                <ul className="col-md-8 d-flex flex-row align-items-center mb-0">
-                    <li className="navitems align-self-center col-2"><button className="btn btn-warning col-12">Documentation</button></li>
-                    <li className="navitems col-2"><button className="btn btn-warning col-12">Reset</button></li>
-                    <li className="navitems col-2"><button className="btn btn-warning col-12">Save</button></li>
-                </ul>
-                </nav>
+                    <nav className="col-md-12 bg-dark d-flex flex-row align-content-center">
+                    <h1 className="col-md-4 ">MarkDown Editor</h1>
+                    <ul className="col-md-8 d-flex flex-row align-items-center mb-0">
+                        <li className="navitems align-self-center col-2"><button className="btn btn-warning col-12" onClick={this.props.action}>Documentation</button></li>
+                        <li className="navitems col-2"><button className="btn btn-warning col-12">Reset</button></li>
+                        <li className="navitems col-2"><button className="btn btn-warning col-12">Save</button></li>
+                    </ul>
+                    </nav>
         )
     }
 }
