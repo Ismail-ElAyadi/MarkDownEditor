9c2cd651f034e81e69be0e11e61c188deb056b6a
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-25 07:49:33 +0200

    Create a state and a Method , and get in into compenent

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Footer from './component/Footer'
 import Body from './component/Body'
 
 export default class App extends Component {
+  state = {
+    documentation:false
+  }
+  display=()=>{
+    this.setState({documentation: !this.state.documentation}
+    )
+  }
   render() {
+    const {documentation} = this.state
     return (
       <Fragment>
-        <Header/>
-        <Body/>
+        <Header action={this.display}/>
+        <Body doc={documentation}/>
         <Footer/>
       </Fragment>
     )
