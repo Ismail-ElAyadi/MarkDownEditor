0fe0730ad4674d3bc1d04a7aa5a6c0a386d9d260
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-25 07:50:06 +0200

    D

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -15,6 +15,20 @@ export default class Body extends Component {
   }
   render() {
     let {text} = this.state
+    let preview =
+    <div className="col-md-5 block">
+    <h2>Preview</h2>
+          <div dangerouslySetInnerHTML= {{__html:this.renderText(text)}}
+            className="col-md-12 preview">
+          </div>
+    </div>
+
+    let iframeDocumentation =
+    <div className="col-md-5 block">
+    <h2>Documentation</h2>
+    <iframe id="documentation" className="col-md-12 preview" title="Markdown's Documentation" src="https://guides.github.com/features/mastering-markdown/"></iframe>
+    </div>
+
     return (
       <section className="p-0 d-flex">
         <div className="col-md-5  block">
@@ -26,13 +40,7 @@ export default class Body extends Component {
           >
           </textarea>
         </div>
-        <div className="col-md-5 block">
-          <h2>Preview</h2>
-          <div dangerouslySetInnerHTML= {{__html:this.renderText(text)}}
-            className="col-md-12 preview"
-          >
-          </div>
-        </div>
+        {this.props.doc ? iframeDocumentation : preview}
       </section>
     );
   }
